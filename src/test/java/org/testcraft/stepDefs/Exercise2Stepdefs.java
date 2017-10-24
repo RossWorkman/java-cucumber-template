d36e35320e20423f57f23a179caf2bb3f3b0adaf
Ross.Workman <Teamfortress2>
2017-10-24 15:28:27 +0100

    Second exercise with extra code

--- src/test/java/org/testcraft/stepDefs/Exercise2Stepdefs.java
+++ src/test/java/org/testcraft/stepDefs/Exercise2Stepdefs.java
@@ -1,6 +1,7 @@
 package org.testcraft.stepDefs;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
@@ -9,6 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Exercise2Stepdefs {
@@ -16,9 +18,10 @@ public class Exercise2Stepdefs {
     public WebDriver driver;
     public WebDriverWait waiter;
 
-    public Exercise2Stepdefs() {
+    private Exercise2Stepdefs() {
         driver = Hooks.driver;
     }
+
 
     @Given("^I'm on https://www\\.thetrainline\\.com/ main page$")
     public void openTrainlinePage () throws Throwable {
@@ -33,23 +36,47 @@ public class Exercise2Stepdefs {
         origin.sendKeys(arg1);
     }
 
-    @When("^I enter destination station \"([^\"]*)\"$")
+    @And("^I enter destination station \"([^\"]*)\"$")
     public void enterDestinationStation (String arg1) throws Throwable {
         WebElement destination = driver.findElement(By.id("destinationStation"));
         destination.sendKeys(arg1);
     }
 
+    @And("^One way is selected$")
+    public void checkOneWay() throws Throwable {
+        if(driver.findElement(By.id("journey-type-single")).isSelected()){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+    }
+
+    @And("^I choose to Arrive by$")
+    public void selectArriveBy() throws Throwable {
+        Select dropdown = new Select(driver.findElement(By.id("outDepartOrArrive")));
+        dropdown.selectByIndex(1);
+
+    }
+
     @When("^I click search$")
-    public void i_click_search() throws Throwable {
+    public void pressSearch() throws Throwable {
         WebElement sendButton = driver.findElement(By.id("submitButton"));
         sendButton.click();
 
     }
 
-    @Then("^train times #tickets should be displayed$")
+    @Then("^train times should be displayed$")
     public void displayTrainTickets() throws Throwable {
         Thread.sleep(3000);
         WebElement tickets = driver.findElement(By.id("tickets"));
         Assert.assertTrue(tickets.isDisplayed());
     }
+
+    @And("^I should have the option to change my choices$")
+    public void cboiceButtonPresent() throws Throwable {
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"changeJourney\"]"));
+        Boolean buttonExists = button.isDisplayed();
+    }
 }
+
+
